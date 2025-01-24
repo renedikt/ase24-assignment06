182830ffc7963df4fde252482c8df6edba4b7835
Benedikt Rief <rief.benedikt@gmail.com>
2025-01-24 02:31:15 +0100

    Remove unused import statement

--- data/src/main/java/de/unibayreuth/se/taskboard/data/impl/UserPersistenceServiceEventSourcingImpl.java
+++ data/src/main/java/de/unibayreuth/se/taskboard/data/impl/UserPersistenceServiceEventSourcingImpl.java
@@ -15,7 +15,6 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-import java.util.function.Supplier;
 
 @Service
 @RequiredArgsConstructor
